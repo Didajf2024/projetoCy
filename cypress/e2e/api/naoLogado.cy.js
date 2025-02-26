4b64ca7ad701b6bda0b92d8a1c9d6b8cfdc3540c
Didajf2024 <diegojf09@hotmail.com>
2025-02-26 19:41:31 -0300

    primeiro commit cypress

--- cypress/e2e/api/naoLogado.cy.js
+++ cypress/e2e/api/naoLogado.cy.js
@@ -9,15 +9,15 @@ describe('API - Profile', () => {
             cy.request({
                 method: 'GET',
                 url: '/api/profile'
-            }).then(respostaAPI =>{
-                expect(respostaAPI.status).to.eq(200)
-                expect(respostaAPI.duration).to.be.lessThan(10000)
-                expect(respostaAPI.body[0].status).to.eq('Gerente de Testes')
-                expect(respostaAPI.body[0].user.name).to.eq('Pedro Guerra')
-                expect(respostaAPI.body[0].skills[0]).to.eq('Cypress')
-                expect(respostaAPI.body[0].skills).to.have.lengthOf(1)
-                expect(respostaAPI.body[0].date).to.not.be.null
-                expect(respostaAPI.headers['x-powered-by']).to.eq('Express')
+            }).then(({ status, duration, body, headers}) =>{
+                expect(status).to.eq(200)
+                expect(duration).to.be.lessThan(10000)
+                expect(body[0].status).to.eq('Gerente de Testes')
+                expect(body[0].user.name).to.eq('Pedro Guerra')
+                expect(body[0].skills[0]).to.eq('Cypress')
+                expect(body[0].skills).to.have.lengthOf(1)
+                expect(body[0].date).to.not.be.null
+                expect(headers['x-powered-by']).to.eq('Express')
             })
         })
     })  
@@ -30,10 +30,39 @@ describe('API - Profile', () => {
                 method: 'GET',
                 url: '/api/profile/user/1',
                 failOnStatusCode: false
-            }).then(respostaAPI => {
-                expect(respostaAPI.status).to.eq(404)
-                expect(respostaAPI.body.errors[0].mensagem).to.eq('perfil nao encontrado')
+            }).then(({status, body}) => {
+                expect(status).to.eq(404)
+                expect(body.errors[0].mensagem).to.eq('perfil nao encontrado')
+            })
+        })
+
+
+        it ('valida um usuario valido',() =>{
+            let usuario = '637d72b91fb5cb0015a02259'
+
+            cy.request({
+                method: 'GET',
+                url: `/api/profile/user/${usuarioId}`
+            }).then(({status, body }) => {
+                expect(status).to.eq(200)
+            })  
+        })
+
+        it('valida um usuario valido buscando na base', () => {
+
+            cy.request({
+                method: 'GET',
+                url: '/api/profile'  
+            }).then(({body }) => {
+
+                cy.request({
+                    method: 'GET',
+                    url: `/api/profile/user/${body[1].user._id}`
+                }).then(({status, body}) =>{
+                    expect(status).to.eq(200)
+                    expect(body.status).to.eq('Outro')
+                })
             })
         })
     })
-})100
+})
